583c650c3c268436dbff418d032c69ab7e97288c
Tien Tran <titran06@gmail.com>
2019-12-05 20:38:13 +0700

    Finished DropdownList

--- Selenium/Webdriver_API/Topic_05_DropdownList.java
+++ Selenium/Webdriver_API/Topic_05_DropdownList.java
@@ -6,18 +6,27 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_05_DropdownList1 {
+public class Topic_05_DropdownList {
 	WebDriver driver;
+	WebDriverWait waitExplicit;
+	Actions action;
 	Select select;
+	JavascriptExecutor javascript;
+	By numberAllItems = By.xpath("//ul[@id ='number-menu']/li");
 
 	// Input data for TC02
 	String firstName = "Automation";
@@ -31,6 +40,9 @@ public class Topic_05_DropdownList1 {
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
+		javascript = (JavascriptExecutor) driver;
+		waitExplicit = new WebDriverWait(driver, 10);
+		action = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
@@ -171,6 +183,134 @@ public class Topic_05_DropdownList1 {
 		Assert.assertTrue(driver.findElement(By.xpath("//a[@class ='ico-account' and text() ='My account']")).isDisplayed());
 		Assert.assertTrue(driver.findElement(By.xpath("//a[@class = 'ico-logout' and text() = 'Log out']")).isDisplayed());
 
+	}
+
+	@Test
+	public void TC_03_dropDownListJQuery() {
+
+		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
+
+		// 1 - Click vào thẻ chứa dropdown để nó xổ ra hết tất cả item
+		driver.findElement(By.xpath("//span[@id = 'number-button']")).click();
+
+		// 2 - Khai báo 1 List WebElement chứa all các items bên trong
+		List<WebElement> allItems = driver.findElements(numberAllItems);
+
+		// 3 - Wait cho tất cả item (List WebElement) được xuất hiện dưới DOM
+		waitExplicit.until(ExpectedConditions.presenceOfAllElementsLocatedBy(numberAllItems));
+
+		// 4 - Get text từng item sau đó so sánh vs item mình cần chọn
+		for (WebElement item : allItems) {
+			System.out.println("Text =" + item.getText());
+
+			// 5 - Kiểm tra item nào đúng vs cái mình cần chọn thì click vào
+			if (item.getText().equals("8")) {
+				item.click();
+				break;
+			}
+		}
+
+		// 6 - Kiểm tra Item đã dc chọn thành công
+
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@id ='number-button']/span[@class ='ui-selectmenu-text' and text()='8']")).isDisplayed());
+
+	}
+
+	@Test
+	public void TC_03_JQuery2() {
+		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
+		// chon Item 19
+		selectCustomDropdownList("//span[@id = 'number-button']", "//ul[@id = 'number-menu']/li", "19");
+
+		// 6 - Kiểm tra Item đã dc chọn thành công
+		Assert.assertTrue(isDisplayed("//span[@id ='number-button']/span[@class ='ui-selectmenu-text' and text()='19']"));
+		
+	}
+
+	@Test
+	public void TC_04_ReactJS() {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+		selectCustomDropdownList("//div[@role = 'listbox']", "//div[@role ='option']", "Christian");
+		Assert.assertTrue(isDisplayed("//div[@role ='listbox']/div[text() ='Christian']"));
+		
+	}
+
+	// Editable Dropdown
+	@Test
+	public void TC_05_ReactJSEditable() throws InterruptedException {
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-search-selection/");
+
+		// Input Algeria
+		inputItemInCustomDropdown("//i[@class = 'dropdown icon']", "//input[@class = 'search']", "Algeria");
+		System.out.println("text = " + driver.findElement(By.xpath("//div[@role ='alert' and text()= 'Algeria']")).getText());
+		Assert.assertTrue(isDisplayed("//div[@role ='alert' and text()= 'Algeria']"));
+		Thread.sleep(3000);
+	}
+	@Test
+	public void TC_05_JQueryEditable() throws InterruptedException {
+		driver.get("http://indrimuska.github.io/jquery-editable-select/");
+		inputItemInCustomDropdown("//div[@id ='default-place']/input", "//div[@id ='default-place']/input", "Alfa Romeo");
+		Assert.assertTrue(isDisplayed("//ul[@class ='es-list']/li[@class = 'es-visible selected' and text() ='Alfa Romeo']"));
+	}
+	@Test
+	public void TC_06_Angular() throws Exception {
+		driver.get("https://ej2.syncfusion.com/angular/demos/?_ga=2.262049992.437420821.1575083417-524628264.1575083417#/material/drop-down-list/data-binding");
+		
+		selectCustomDropdownList("// div[@id ='local']//span[contains(@class,'e-search-icon')]", "//ul[@id ='games_options']/li", "Basketball");
+		String dropDownText = getTextbyJS("#games_hidden > option");
+		Assert.assertEquals(dropDownText, "Basketball");
+		Thread.sleep(3000);
+		
+
+	}	
+	
+	public void selectCustomDropdownList(String parentXpath, String allItemsXpath, String expectedText) {
+
+		// 1 - Click vào thẻ chứa dropdown để nó xổ ra hết tất cả item
+		driver.findElement(By.xpath(parentXpath)).click();
+
+		// 2 - Khai báo 1 List WebElement chứa all các items bên trong
+		List<WebElement> allItems = driver.findElements(By.xpath(allItemsXpath));
+
+		// 3 - Wait cho tất cả item (List WebElement) được xuất hiện dưới DOM
+		waitExplicit.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemsXpath)));
+
+		// 4 - Get text từng item sau đó so sánh vs item mình cần chọn
+		for (WebElement item : allItems) {
+			System.out.println("Text =" + item.getText());
+
+			// 5 - Kiểm tra item nào đúng vs cái mình cần chọn thì click vào
+			if (item.getText().equals(expectedText)) {
+				item.click();
+				break;
+			}
+		}
+
+	}
+
+	public void inputItemInCustomDropdown(String parentXpath, String inputXpath, String expectedText) {
+		// 1 - Click vao the chua Dropdown de no xo xuong
+		driver.findElement(By.xpath(parentXpath)).click();
+
+		// 2 - input Value vao o search
+		driver.findElement(By.xpath(inputXpath)).sendKeys(expectedText);
+
+		// 3 - Truyen phim ENTER vao sau khi input
+		action.sendKeys(driver.findElement(By.xpath(inputXpath)), Keys.ENTER).perform();
+	}
+
+	public String getTextbyJS(String locator) {
+		return (String) javascript.executeScript("return document.querySelector('" + locator + "').text");
+		
+	}
+
+	public boolean isDisplayed(String xpathLocator) {
+		WebElement element = driver.findElement(By.xpath(xpathLocator));
+		if (element.isDisplayed()) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	// Post-Condition
